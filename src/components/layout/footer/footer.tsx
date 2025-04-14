d62e8757baedb91bf28d75475406d64e4ca4aec3
mrhl082024 <mrhl082024@gmail.com>
2025-04-14 09:36:59 +0200

    ADD Footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,7 +1,20 @@
+import locationPin from "./public/lucide_map-pin.png";
 export function Footer() {
   return (
-    <footer className="container flex justify-center">
-      <span>copyright @ kodeverket 2025</span>
+    <footer className="container flex flex-col items-center bg-gray-800">
+      <section>
+        <h2>NAPCRACARE</h2>
+      </section>
+      <section>
+        <p>Valkendorfsgaten 9, Bergen, Norway</p>
+        <p>email</p>
+        <p>phone</p>
+        <p>facebook</p>
+      </section>
+      <section>
+        <p>Utviklet i regnet av Kodeverket Bergen @ 2025</p>
+        <p>copyright @ kodeverket 2025</p>
+      </section>
     </footer>
   );
 }
