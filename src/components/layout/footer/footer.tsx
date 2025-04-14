c1d2476c2f15ff239878db37714304e0c4b59103
mrhl082024 <mrhl082024@gmail.com>
2025-04-14 10:06:12 +0200

    ADD footer

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,4 +1,4 @@
-import locationPin from "./public/lucide_map-pin.png";
+import Image from "next/image";
 export function Footer() {
   return (
     <footer className="container flex flex-col items-center bg-gray-800">
@@ -6,14 +6,26 @@ export function Footer() {
         <h2>NAPCRACARE</h2>
       </section>
       <section>
-        <p>Valkendorfsgaten 9, Bergen, Norway</p>
-        <p>email</p>
-        <p>phone</p>
-        <p>facebook</p>
+        <p className="hover:text-blue-600">
+          <a href="">Facebook</a>
+        </p>
+        <p>951 26 366</p>
+        <p>
+          <Image src="" width={25} height={25} alt="Mail Icon" />
+          napraptklinikkvest@gmail.com
+        </p>
+        <p className="flex flex-row gap-1">
+          <Image
+            src="/footer/lucide_map-pin.png"
+            width={25}
+            height={20}
+            alt="Location Pin"
+          />
+          Valkenfordsgaten 9, Bergen, Norway
+        </p>
       </section>
       <section>
         <p>Utviklet i regnet av Kodeverket Bergen @ 2025</p>
-        <p>copyright @ kodeverket 2025</p>
       </section>
     </footer>
   );
