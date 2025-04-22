7eaa59f3bb7e6a28a64eddcd00bf2edf6b12d2fa
mrhl082024 <mrhl082024@gmail.com>
2025-04-22 08:58:31 +0200

    removed lines that we're never used

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "@/assets/styles/globals.css";
 import { Header } from "@/components/layout/header/header";
 import { Footer } from "@/components/layout/footer/footer";
-import { Map } from "@/components/map";
 export const metadata: Metadata = {
   title: "Tempalte page",
   description: "Kodeverket sin NextJS template",
