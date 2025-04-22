da2dac1ec65f823c736cdef4621fc4fa897ea08e
mrhl082024 <mrhl082024@gmail.com>
2025-04-22 10:17:54 +0200

    cropped image

--- src/components/hero/mobileHero.tsx
+++ src/components/hero/mobileHero.tsx
@@ -7,15 +7,16 @@ export function MobileHero() {
         <h2>Vi lindrer smertene dine på en profosjonell måte</h2>
         <p>Veien til helse starter her - registrer deg nå.</p>
       </section>
-      <section className="h-[900px] ">
+      <section className="h-[360px] ">
         <Image
           className="position fixed"
           src="/doctor-with-crossed-arms.png"
-          width={500}
-          height={500}
+          width={100}
+          height={360}
           alt="doctor with crossed arms"
         />
-        <div className="bg-blue-500 w-screen h-[100px]"></div>
+        <div className="w-screen h-[260px]"></div>
+        <div className="bg-blue-950 w-screen h-[100px]"></div>
       </section>
     </div>
   );
