f2dd565b656e00dd74e3bd448ebcd1fd3d834e6b
mrhl082024 <mrhl082024@gmail.com>
2025-04-22 11:44:08 +0200

    ongoing css for mobile

--- src/components/hero/mobileHero.tsx
+++ src/components/hero/mobileHero.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-
+import { ArrowRight } from "lucide-react";
 export function MobileHero() {
   return (
-    <div className="bg-white w-screen text-black">
-      <section>
-        <h2>Vi lindrer smertene dine på en profosjonell måte</h2>
-        <p>Veien til helse starter her - registrer deg nå.</p>
+    <div className="bg-white w-screen">
+      <section className="text-zinc-700 w-[360px]">
+        <h2 className="text-4xl font-bold mt-5 mx-4 mb-3">
+          Vi lindrer smertene dine på en profosjonell måte
+        </h2>
+        <p className="text-xl font-semibold mx-6 mb-10">
+          Veien til helse starter her
+          <br />- registrer deg nå.
+        </p>
+        <button className="flex justify-center items-center gap-1 bg-main-500 w-[320px] h-[56px] mx-5 p-1 rounded-xl text-white text-lg">
+          Bestill time <ArrowRight />
+        </button>
       </section>
       <section className="flex items-center flex-col h-[648px] ">
         <Image
@@ -16,7 +24,7 @@ export function MobileHero() {
           alt="doctor with crossed arms"
         />
         <div className="w-screen h-[548px]"></div>
-        <div className="bg-blue-950 w-screen h-[100px]"></div>
+        <div className="bg-main-500 w-screen h-[100px]"></div>
       </section>
     </div>
   );
