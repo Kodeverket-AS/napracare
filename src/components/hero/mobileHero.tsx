14a69ac6441b17ff922842a033fa49e69a24da96
mrhl082024 <mrhl082024@gmail.com>
2025-04-22 10:00:24 +0200

    added Image

--- src/components/hero/mobileHero.tsx
+++ src/components/hero/mobileHero.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
+
 export function MobileHero() {
   return (
-    <>
+    <div className="bg-white w-screen text-black">
       <section>
         <h2>Vi lindrer smertene dine på en profosjonell måte</h2>
         <p>Veien til helse starter her - registrer deg nå.</p>
       </section>
-    </>
+      <section className="h-[900px] ">
+        <Image
+          className="position fixed"
+          src="/doctor-with-crossed-arms.png"
+          width={500}
+          height={500}
+          alt="doctor with crossed arms"
+        />
+        <div className="bg-blue-500 w-screen h-[100px]"></div>
+      </section>
+    </div>
   );
 }
