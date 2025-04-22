006b570a331ee97308ab5af917a9d2ab6ae39fd3
mrhl082024 <mrhl082024@gmail.com>
2025-04-22 14:51:22 +0200

    started on the html

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -1,7 +1,22 @@
 export function Reviews() {
   return (
     <>
-      <p>test</p>
+      <section className="w-[345px] h-[216px]">
+        <h1>Hva sier kundene?</h1>
+        <p>placeholder gibberish</p>
+        <div className="w-[345px] h-[56px] flex flex-row">
+          <button>venstre</button>
+          <button>høyre</button>
+        </div>
+      </section>
+      <section className="w-[345px] h-[312px]">
+        <p>review text from customer</p>
+        <div>
+          <img src="" alt="picture" />
+          <p>name</p>
+          <p>date</p>
+        </div>
+      </section>
     </>
   );
 }
