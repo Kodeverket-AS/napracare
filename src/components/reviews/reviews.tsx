0e1ef346cef564f8c96ac85e8b7093ac902cb972
mrhl082024 <mrhl082024@gmail.com>
2025-04-23 10:46:48 +0200

    review css size md

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -3,17 +3,17 @@ import { ArrowLeft, ArrowRight, UserRound } from "lucide-react";
 export function Reviews() {
   return (
     <div className="bg-white w-full">
-      <section className="text-main-500 mx-5 mt-8 sm:mx-10 sm:mt-16">
+      <section className="text-main-500 mx-5 mt-8 sm:mx-10 sm:mt-16 ">
         <h1 className="text-[48px]/[110%] font-[600] mb-5">
           Hva sier kundene?
         </h1>
-        <p className="text-[16px]/150% font-[400] mb-5">
+        <p className="text-[16px]/150% font-[400] mb-5 md:w-[640px]">
           Her får du hjelp til dine akutte og langvarige problemer gjennom
           manuell behandling som sørger for å minske smerte og gjenskape
           kroppens naturlige funksjon og bevegelighet. Hos oss får du time kjapt
           slik at du kommer raskt tilbake fra din skade.
         </p>
-        <div className="flex justify-between h-[56px] flex flex-row mb-5">
+        <div className="flex justify-between h-[56px] flex flex-row mb-5 sm:justify-start sm:gap-10">
           <button className="flex justify-center items-center rounded-full shadow-2xl shadow-black w-[56px] h-[56px]">
             <ArrowLeft size={40} />
           </button>
@@ -22,7 +22,7 @@ export function Reviews() {
           </button>
         </div>
       </section>
-      <section className="flex flex-col justify-between min-h-[312px] bg-main-500 text-white mt-10 mx-5 mb-20 p-5 rounded-xl sm:mx-10 sm:min-h-[240px]">
+      <section className="flex flex-col justify-between min-h-[312px] bg-main-500 text-white mt-10 mx-5 mb-20 p-5 rounded-xl sm:mx-10 sm:min-h-[240px] md:w-[675px]">
         <p>
           Kan virkelig varmt anbefale Ørjan for de som kjenner at de trenger
           litt "service"! Han har en egen evne til å få koppen på plass og i
