90d716a146f5bbb57002a88057b19fc6ff010a53
mrhl082024 <mrhl082024@gmail.com>
2025-05-07 23:00:38 +0200

    changed folder name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { Hero } from "@/components/hero/hero";
 import { Reviews } from "@/components/reviews/reviews";
 import { Map } from "@/components/map";
-import { Naprapati } from "@/components/Hva_Er/Naprapati";
+import { Naprapati } from "@/components/whatIsNaprapat/Naprapati";
 import { Services } from "@/components/services/services";
 
 export default function Home() {
