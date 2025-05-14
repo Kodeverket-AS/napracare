8840e6faa358024c2d4795183bded7be9a48f98e
mrhl082024 <mrhl082024@gmail.com>
2025-05-14 14:41:45 +0200

    changed width

--- src/components/hero/mobileHero.tsx
+++ src/components/hero/mobileHero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { ArrowRight } from "lucide-react";
 export function MobileHero() {
   return (
-    <div className="bg-white w-screen">
+    <div className="bg-white w-[calc(100vw-15px)]">
       <section className="flex justify-center items-center text-zinc-700">
         <div className="w-[360px] sm:w-[640px] gap-[24px]">
           <h2 className="text-[56px]/[110%] font-[600] mx-5 mt-5 mb-[24px]">
@@ -16,7 +16,7 @@ export function MobileHero() {
           </button>
         </div>
       </section>
-      <section className="flex flex-col justify-end items-center h-[660px] ">
+      <section className="flex flex-col justify-end items-center h-[661px] ">
         <Image
           className="flex position absolute h-[633px]"
           src="/doctor-with-crossed-arms.png"
@@ -24,8 +24,8 @@ export function MobileHero() {
           height={648}
           alt="doctor with crossed arms"
         />
-        <div className="w-screen h-[548px]"></div>
-        <div className="bg-main-500 w-screen h-[100px]"></div>
+        <div className="w-full h-[549px]"></div>
+        <div className="bg-main-500 w-full h-[112px]"></div>
       </section>
     </div>
   );
