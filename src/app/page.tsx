ecbc8dc0598992c96e703d7f54b0280df405e59c
mrhl082024 <mrhl082024@gmail.com>
2025-05-15 10:34:47 +0200

    added container to page. and moved Map elsewhere so it wont be affected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 import { Hero } from "@/components/hero/hero";
 import { Reviews } from "@/components/reviews/reviews";
-import { Map } from "@/components/map";
 import { Naprapati } from "@/components/whatIsNaprapat/Naprapati";
 import { Services } from "@/components/services/services";
 
 export default function Home() {
   return (
-    <>
+    <div className="w-full container mx-auto px-8">
       <Hero />
       <Naprapati />
       <Services />
       <Reviews />
-      <Map />
-    </>
+    </div>
   );
 }
