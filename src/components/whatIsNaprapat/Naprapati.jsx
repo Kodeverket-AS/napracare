689231a08e49b75baa77ef55bed75d2a11afb0a0
mrhl082024 <mrhl082024@gmail.com>
2025-05-19 11:48:39 +0200

    no longer giant spaces between words

--- src/components/whatIsNaprapat/Naprapati.jsx
+++ src/components/whatIsNaprapat/Naprapati.jsx
@@ -19,8 +19,10 @@ export function Naprapati() {
       </div>
       <div className="lg:w-1/2 w-full  flex flex-col px-4 lg:px-0 bg-main-100">
         <div className="m-12 ">
-          <h1 className=" lg:mt-0 text-[48px] font-DMSans text-main-500 font-semibold mb-12">Hva er naprapati?</h1>
-          <p className="mt-3 text-justify flex-grow text-dark-text">
+          <h1 className=" lg:mt-0 text-[48px] font-DMSans text-main-500 font-semibold mb-12">
+            Hva er naprapati?
+          </h1>
+          <p className="mt-3 flex-grow text-dark-text">
             Behandlingen bygger på ortopedisk medisin kombinert med god kunnskap
             om bevegelsesapparatets oppbygning og funksjon. Denne tanken ligger
             til grunn for naprapatien og kan betegnes som selve kjernen. Ordet
@@ -34,14 +36,13 @@ export function Naprapati() {
             smertetilstander i muskel- og leddsystemet.
             <br />
             <br />
-            Som du vil forstå gjennom historien er naprapati ikke noe nytt påfunn
-            eller har oppstått som en blanding av forskjellige behandlingsformer,
-            men har alltid vært et eget selvstendig yrke, som i utgangspunktet har
-            hatt sitt eget diagnostiske system og egne behandlingsteknikker.
+            Som du vil forstå gjennom historien er naprapati ikke noe nytt
+            påfunn eller har oppstått som en blanding av forskjellige
+            behandlingsformer, men har alltid vært et eget selvstendig yrke, som
+            i utgangspunktet har hatt sitt eget diagnostiske system og egne
+            behandlingsteknikker.
           </p>
-          <button 
-            className="flex justify-center items-center gap-4 bg-main-500 hover:bg-main-400 w-full max-w-[320px] sm:max-w-[188px] h-[56px] mx-auto mt-6 mb-[40px] rounded-[8px] text-white text-lg cursor-pointer"
-          >
+          <button className="flex justify-center items-center gap-4 bg-main-500 hover:bg-main-400 w-full max-w-[320px] sm:max-w-[188px] h-[56px] mx-auto mt-6 mb-[40px] rounded-[8px] text-white text-lg cursor-pointer">
             Bestill time <ArrowRight />
           </button>
         </div>
